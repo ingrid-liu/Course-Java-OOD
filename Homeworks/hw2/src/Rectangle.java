1d58700edca307edf425c7dfed40ce8b406fd4d0
AnnieBox <todearannie@gmail.com>
2021-06-13 11:41:55 -0700

    hw2 submissionn

--- Homeworks/hw2/src/Rectangle.java
+++ Homeworks/hw2/src/Rectangle.java
@@ -74,11 +74,6 @@ public class Rectangle {
 
     public Rectangle union(Rectangle other) {
         Rectangle unionRec = new Rectangle(0,0,0,0);
-//        unionRec.x = this.x < other.x ? this.x : other.x;
-//        unionRec.y = this.y < other.y ? this.y : other.y;
-//        double unionRecEndX = this.x + this.w > other.x + other.w ? this.x + this.w :other.x + other.w;
-//        double unionRecEndY = this.y + this.h > other.y + other.h ? this.y + this.h : other.y + other.h;
-
         unionRec.x = Math.min(this.x, other.x);
         unionRec.y = Math.min(this.y, other.y);
         int unionRecEndX = Math.max(this.x + this.w, other.x + other.w);
@@ -124,101 +119,6 @@ public class Rectangle {
 
     public void setH(int h) {
         this.h = h;
-    }
-
-
-
-
-
-
-    public static void main(String[] args) {
-//        double x1 = 3;  double y1 = 3;  double w1 = 3;  double h1 = 3;
-//        double x2 = 2;  double y2 = 5;  double w2 = 2;  double h2 = 2;
-//        double x3 = 5;  double y3 = 5;  double w3 = 2;  double h3 = 2;
-//        double x4 = 5;  double y4 = 2;  double w4 = 2;  double h4 = 2;
-//        double x5 = 2;  double y5 = 2;  double w5 = 2;  double h5 = 2;
-//        double x6 = 2;  double y6 = 2;  double w6 = 5;  double h6 = 5;
-//        double x7 = 4;  double y7 = 4;  double w7 = 1;  double h7 = 1;
-//        double x8 = 6;  double y8 = 6;  double w8 = 3;  double h8 = 3;
-//
-//        double x9 = 2;  double y9 = 4;  double w9 = 2;  double h9 = 1;
-//        double x0 = 2;  double y0 = 2;  double w0 = 2;  double h0 = 5;
-//        double xi = 2;  double yi = 3;  double wi = 1;  double hi = 3;
-
-        // test negative
-//        double xii = -1;  double yii = -2;  double wii = 7;  double hii = 6;
-//        double xiii = 0;  double yiii = 0;  double wiii = 3;  double hiii = 2;
-//        double xiv = -2; double yiv = -2; double wiv = 2; double hiv = 3;
-
-
-
-//        Rectangle r1 = new Rectangle(x1, y1, w1, h1);       // ← this       ↓ other
-//        Rectangle r2 = new Rectangle(x2, y2, w2, h2);       // overlap1 other: upper-left
-//        Rectangle r3 = new Rectangle(x3, y3, w3, h3);       // overlap1 other: upper-right
-//        Rectangle r4 = new Rectangle(x4, y4, w4, h4);       // overlap1 other: lower-right
-//        Rectangle r5 = new Rectangle(x5, y5, w5, h5);       // overlap1 other: lower-left
-//        Rectangle r6 = new Rectangle(x6, y6, w6, h6);       // fully contains       expected:(3,3,3,3)
-//        Rectangle r7 = new Rectangle(x7, y7, w7, h7);       // fully contained      expected:(4,4,1,1)
-//        Rectangle r8 = new Rectangle(x8, y8, w8, h8);       // no overlap or intersection
-//
-//        // three more corner cases
-//        Rectangle r9 = new Rectangle(x9, y9, w9, h9);        // partially contains  expected:(3,4,1,1)
-//        Rectangle r0 = new Rectangle(x0, y0, w0, h0);        // partially contains  expected:(3,3,1,3)
-//        Rectangle ri = new Rectangle(xi, yi, wi, hi);        // partially contains  expected: No overlap
-
-        // test negative coordinates
-//        Rectangle rii = new Rectangle(xii, yii, wii, hii);
-//        Rectangle riii = new Rectangle(xiii, yiii, wiii, hiii);
-//        Rectangle riv = new Rectangle(xiv, yiv, wiv, hiv);
-
-//        System.out.println("-------------------Test toString:");
-//        System.out.println("Rectangle1:\n" + r1);
-//        System.out.println("Rectangle2:\n" + r2);
-//        System.out.println("Rectangle3:\n" + r3);
-//        System.out.println("Rectangle4:\n" + r4);
-//        System.out.println("Rectangle5:\n" + r5);
-
-//        System.out.println("Rectangle8\n" + r8);
-        System.out.println("\n-------------------Test overlapped:");
-        Rectangle rectangle1 = new Rectangle(-2,-2,4,4);
-        Rectangle rectangleNW = new Rectangle(-3,1,2,2);
-        System.out.println(rectangle1.overlap(rectangleNW));
-//        System.out.println(r1.overlap(r2));
-//        System.out.println(r1.overlap(r3));
-//        System.out.println(r1.overlap(r4));
-//        System.out.println(r1.overlap(r5));
-//        System.out.println(r1.overlap(r6));
-//        System.out.println(r1.overlap(r7));
-//        System.out.println(r1.overlap(r8));
-//        System.out.println(r1.overlap(r9));
-//        System.out.println(r1.overlap(r0));
-//        System.out.println(r1.overlap(ri));
-//        System.out.println(rii.overlap(riii));
-
-        System.out.println("\n-------------------Test intersect:");
-//        System.out.println(r1.intersect(r2));
-//        System.out.println(r1.intersect(r3));
-//        System.out.println(r1.intersect(r4));
-//        System.out.println(r1.intersect(r5));
-//        System.out.println(r1.intersect(r6) + "  --- expected:(3,3,3,3)");
-//        System.out.println(r1.intersect(r7) + "  --- expected:(4,4,1,1)");
-//        // System.out.println(r1.intersect(r8));
-//
-//        System.out.println(r1.intersect(r9) + "  --- expected:(3,4,1,1)");
-//        System.out.println(r1.intersect(r0) + "  --- expected:(3,3,1,3)");
-//        //System.out.println(r1.intersect(ri));
-//        System.out.println(rii.intersect(riii) + "  --- expected:(0,0,3,2)");
-//        System.out.println(rii.intersect(riv) + "  --- expected:(-1,-2,1,3)");
-
-        System.out.println("\n-------------------Test union:");
-//        System.out.println(r1.union(r2)+ "  --- expected:(2,3,4,4)");
-//        System.out.println(r1.union(r3)+ "  --- expected:(3,3,4,4)");
-//        System.out.println(r1.union(r4)+ "  --- expected:(3,2,4,4)");
-//        System.out.println(r1.union(r5)+ "  --- expected:(2,2,4,4)");
-//        System.out.println(r1.union(r6)+ "  --- expected:(2,2,5,5)");
-//        System.out.println(r1.union(r7)+ "  --- expected:(3,3,3,3)");
-//        System.out.println(rii.union(riii)+ "  --- expected:(-1,-2,7,6)");
-//        System.out.println(rii.union(riv)+ "  --- expected:(-2,-2,8,6)");
     }
 
 }
--- Homeworks/hw2/test/TestRectangle.java
+++ Homeworks/hw2/test/TestRectangle.java
@@ -1,6 +1,5 @@
 import jdk.swing.interop.SwingInterOpUtils;
 import org.junit.*;
-
 import static org.junit.Assert.*;
 
 public class TestRectangle {
@@ -14,11 +13,38 @@ public class TestRectangle {
     private Rectangle rectangleSE;
     private Rectangle rectangleSW;
 
-//    private Rectangle rectangle;
+    private Rectangle parallelPassThrough;
+    private Rectangle verticalPassThrough;
+    private Rectangle almostCover1;
+    private Rectangle almostCover2;
 
-    /*
-    :rectangleOriginal: original rectangle across both positive and negative coordinates
-    :rectangle: N, S, W, E, NW, NE, SE, SW: partially overlapped with the rectangleOriginal
+    private Rectangle unOverlappedN;
+    private Rectangle unOverlappedS;
+    private Rectangle unOverlappedW;
+    private Rectangle unOverlappedE;
+    private Rectangle unOverlappedNW;
+    private Rectangle unOverlappedNE;
+    private Rectangle unOverlappedSW;
+    private Rectangle unOverlappedSE;
+
+    private Rectangle touch1edge;
+    private Rectangle touch2edge;
+    private Rectangle touch3edge;
+
+    private Rectangle touchNodeNE;
+    private Rectangle touchNodeNW;
+    private Rectangle touchNodeSE;
+    private Rectangle touchNodeSW;
+
+    private Rectangle innerRectangle;
+    private Rectangle outerRectangle;
+
+
+
+    /**
+     * myTestSetUp()
+     * :rectangleOriginal: original rectangle across both positive and negative coordinates
+     * :rectangle_ N, S, W, E, NW, NE, SE, SW: partially overlapped with the rectangleOriginal
      */
     @Before
     public void myTestSetUp() throws Exception {
@@ -31,32 +57,62 @@ public class TestRectangle {
         rectangleNE = new Rectangle(1,1,5,4);
         rectangleSE = new Rectangle(1,-3, 2,2);
         rectangleSW = new Rectangle(-4,-5,3,4);
+
+        parallelPassThrough = new Rectangle(-5, -1, 10,2);
+        verticalPassThrough = new Rectangle(-1, -5, 2,10);
+        almostCover1 = new Rectangle(-1,-3,10,10);
+        almostCover2 = new Rectangle(-5,-1,10,10);
+
+        unOverlappedN = new Rectangle(-2, -4, 1,1);
+        unOverlappedS = new Rectangle(0, 4, 2,1);
+        unOverlappedW = new Rectangle(-4, -1, 1,4);
+        unOverlappedE = new Rectangle(3, -2, 2,10);
+        unOverlappedNW = new Rectangle(-8, -8, 2,2);
+        unOverlappedNE = new Rectangle(8, 8, 2,2);
+        unOverlappedSW = new Rectangle(-8, -8, 2,2);
+        unOverlappedSE = new Rectangle(8, -8, 2,2);
+
+        touch1edge = new Rectangle(-4,-2,2,4);
+        touch2edge = new Rectangle(-4,-5,6,8);
+        touch3edge = new Rectangle(-2,-2,6,6);
+
+
+        touchNodeNE = new Rectangle(2,2,2,2);
+        touchNodeNW = new Rectangle(-4,2,2,2);
+
+        touchNodeSE = new Rectangle(2,-4,2,2);
+        touchNodeSW = new Rectangle(-4,-4,2,2);
+//
+        innerRectangle = new Rectangle(-1,-1,2,2);
+        outerRectangle = new Rectangle(-4,-4,10,10);
+
     }
 
-    /*
-    Test the partially overlapped rectangles from N/S/E/W directions
+    /**
+     * testNEWSOverlap: test overlapped rectangles with the rectangleOriginal from N, S, E, W directions
      */
     @Test
     public void testNEWSOverlap() throws Exception {
-        assertEquals(true, rectangleOriginal.overlap(rectangleN));
-        assertEquals(true, rectangleOriginal.overlap(rectangleS));
-        assertEquals(true, rectangleOriginal.overlap(rectangleE));
-        assertEquals(true, rectangleOriginal.overlap(rectangleW));
+        assertTrue(rectangleOriginal.overlap(rectangleN));
+        assertTrue(rectangleOriginal.overlap(rectangleS));
+        assertTrue(rectangleOriginal.overlap(rectangleE));
+        assertTrue(rectangleOriginal.overlap(rectangleW));
     }
 
-    /*
-    Test the partially overlapped rectangles from four corner directions
+    /**
+     * test the partially overlapped rectangles from four corner directions
      */
     @Test
     public void testCornerOverlap() throws Exception {
-        assertEquals(true, rectangleOriginal.overlap(rectangleNW));
-        assertEquals(true, rectangleOriginal.overlap(rectangleNE));
-        assertEquals(true, rectangleOriginal.overlap(rectangleSE));
-        assertEquals(true, rectangleOriginal.overlap(rectangleSW));
+        assertTrue(rectangleOriginal.overlap(rectangleNW));
+        assertTrue(rectangleOriginal.overlap(rectangleNE));
+        assertTrue(rectangleOriginal.overlap(rectangleSE));
+        assertTrue(rectangleOriginal.overlap(rectangleSW));
     }
 
-    /*
-    Test the intersection results of the above overlapped cases
+    /**
+     * test the intersection results of the above overlapped cases (N/S/E/W + four corners)
+     * test on getter/setter at the same time
      */
     @Test
     public void testPartiallyOverlappedIntersect() throws Exception {
@@ -89,7 +145,6 @@ public class TestRectangle {
         assertEquals(expectedResultE.getW(), IntersectE.getW());
         assertEquals(expectedResultE.getH(), IntersectE.getH());
 
-        rectangleNW = new Rectangle(-3,1,2,2);
         Rectangle expectedResultNW = new Rectangle(-2,1,1,1);
         Rectangle IntersectNW = rectangleOriginal.intersect(rectangleNW);
         assertEquals(expectedResultNW.getX(), IntersectNW.getX());
@@ -97,7 +152,6 @@ public class TestRectangle {
         assertEquals(expectedResultNW.getW(), IntersectNW.getW());
         assertEquals(expectedResultNW.getH(), IntersectNW.getH());
 
-        rectangleNE = new Rectangle(1,1,5,4);
         Rectangle expectedResultNE = new Rectangle(1,1,1,1);
         Rectangle IntersectNE = rectangleOriginal.intersect(rectangleNE);
         assertEquals(expectedResultNE.getX(), IntersectNE.getX());
@@ -120,21 +174,16 @@ public class TestRectangle {
         assertEquals(expectedResultSW.getH(), IntersectSW.getH());
     }
 
-    /*
-    Test several other partially overlapped cases, i.e. pass through the original rectangle
+    /**
+     * test several other partially overlapped cases, i.e. pass through the original rectangle
     or almost contained the original rectangle
      */
     @Test
     public void testOtherPartiallyOverlapAndIntersect() throws Exception {
-        Rectangle parallelPassThrough = new Rectangle(-5, -1, 10,2);
-        Rectangle verticalPassThrough = new Rectangle(-1, -5, 2,10);
-        Rectangle almostCover1 = new Rectangle(-1,-3,10,10);
-        Rectangle almostCover2 = new Rectangle(-5,-1,10,10);
-
-        assertEquals(true, rectangleOriginal.overlap(parallelPassThrough));
-        assertEquals(true, rectangleOriginal.overlap(verticalPassThrough));
-        assertEquals(true, rectangleOriginal.overlap(almostCover1));
-        assertEquals(true, rectangleOriginal.overlap(almostCover2));
+        assertTrue(rectangleOriginal.overlap(parallelPassThrough));
+        assertTrue(rectangleOriginal.overlap(verticalPassThrough));
+        assertTrue(rectangleOriginal.overlap(almostCover1));
+        assertTrue(rectangleOriginal.overlap(almostCover2));
 
         Rectangle expectedResultParallel = new Rectangle(-2,-1,4,2);
         Rectangle IntersectParallel = rectangleOriginal.intersect(parallelPassThrough);
@@ -151,37 +200,157 @@ public class TestRectangle {
         assertEquals(expectedResultVertical.getH(), IntersectVertical.getH());
 
         Rectangle expectedResultAlmostCover1 = new Rectangle(-1,-2,3,4);
-        Rectangle IntersectAlmost1 = rectangleOriginal.intersect(verticalPassThrough);
-        assertEquals(IntersectAlmost1.getX(), IntersectAlmost1.getX());
-        assertEquals(IntersectAlmost1.getY(), IntersectAlmost1.getY());
-        assertEquals(IntersectAlmost1.getW(), IntersectAlmost1.getW());
-        assertEquals(IntersectAlmost1.getH(), IntersectAlmost1.getH());
-        System.out.println(rectangleOriginal.intersect(verticalPassThrough));
-//        Rectangle almostCover1 = new Rectangle(-1,-3,10,10);
-//        Rectangle almostCover2 = new Rectangle(-5,-1,10,10);
+        Rectangle IntersectAlmost1 = rectangleOriginal.intersect(almostCover1);
+        assertEquals(expectedResultAlmostCover1.getX(), IntersectAlmost1.getX());
+        assertEquals(expectedResultAlmostCover1.getY(), IntersectAlmost1.getY());
+        assertEquals(expectedResultAlmostCover1.getW(), IntersectAlmost1.getW());
+        assertEquals(expectedResultAlmostCover1.getH(), IntersectAlmost1.getH());
 
-        Rectangle expectedResultAlmostCover2 = new Rectangle(-1,-2,2,4);
-        Rectangle IntersectAlmost2 = rectangleOriginal.intersect(verticalPassThrough);
-        System.out.println(rectangleOriginal.intersect(verticalPassThrough));
-//        assertEquals(IntersectAlmost2.getX(), verticalPassThrough.getX());
-//        assertEquals(IntersectAlmost2.getY(), verticalPassThrough.getY());
-//        assertEquals(IntersectAlmost2.getW(), verticalPassThrough.getW());
-//        assertEquals(IntersectAlmost2.getH(), verticalPassThrough.getH());
+        Rectangle expectedResultAlmostCover2 = new Rectangle(-2,-1,4,3);
+        Rectangle IntersectAlmost2 = rectangleOriginal.intersect(almostCover2);
+        assertEquals(expectedResultAlmostCover2.getX(), IntersectAlmost2.getX());
+        assertEquals(expectedResultAlmostCover2.getY(), IntersectAlmost2.getY());
+        assertEquals(expectedResultAlmostCover2.getW(), IntersectAlmost2.getW());
+        assertEquals(expectedResultAlmostCover2.getH(), IntersectAlmost2.getH());
+    }
+
+    /**
+     * test rectangles from all directions without overlapping with the original rectangle
+     */
+    @Test
+    public void testUnOverlapped() throws Exception {
+        assertFalse(rectangleOriginal.overlap(unOverlappedN));
+        assertFalse(rectangleOriginal.overlap(unOverlappedS));
+        assertFalse(rectangleOriginal.overlap(unOverlappedW));
+        assertFalse(rectangleOriginal.overlap(unOverlappedE));
+        assertFalse(rectangleOriginal.overlap(unOverlappedNW));
+        assertFalse(rectangleOriginal.overlap(unOverlappedNE));
+        assertFalse(rectangleOriginal.overlap(unOverlappedSW));
+        assertFalse(rectangleOriginal.overlap(unOverlappedSE));
+    }
+
+    /**
+     * test union result on above all cases
+     * test on toString at the same time
+     */
+    @Test
+    public void testUnion() throws Exception {
+        Rectangle expectedResultN = new Rectangle(-2,-2,4,5);
+        Rectangle expectedResultS = new Rectangle(-2,-3,4,5);
+        Rectangle expectedResultE = new Rectangle(-2,-2,10,4);
+        Rectangle expectedResultW = new Rectangle(-6,-2,8,4);
+
+        assertEquals(expectedResultN.toString(), rectangleOriginal.union(rectangleN).toString());
+        assertEquals(expectedResultS.toString(), rectangleOriginal.union(rectangleS).toString());
+        assertEquals(expectedResultE.toString(), rectangleOriginal.union(rectangleE).toString());
+        assertEquals(expectedResultW.toString(), rectangleOriginal.union(rectangleW).toString());
+
+        assertEquals("x:-2, y:-2, w:8, h:7", rectangleOriginal.union(rectangleNE).toString());
+        assertEquals("x:-3, y:-2, w:5, h:5", rectangleOriginal.union(rectangleNW).toString());
+        assertEquals("x:-2, y:-3, w:5, h:5", rectangleOriginal.union(rectangleSE).toString());
+        assertEquals("x:-4, y:-5, w:6, h:7", rectangleOriginal.union(rectangleSW).toString());
+
+        assertEquals("x:-5, y:-2, w:10, h:4", rectangleOriginal.union(parallelPassThrough).toString());
+        assertEquals("x:-2, y:-5, w:4, h:10", rectangleOriginal.union(verticalPassThrough).toString());
+        assertEquals("x:-2, y:-3, w:11, h:10", rectangleOriginal.union(almostCover1).toString());
+        assertEquals("x:-5, y:-2, w:10, h:11", rectangleOriginal.union(almostCover2).toString());
+
+        assertEquals("x:-2, y:-4, w:4, h:6", rectangleOriginal.union(unOverlappedN).toString());
+        assertEquals("x:-2, y:-2, w:4, h:7", rectangleOriginal.union(unOverlappedS).toString());
+        assertEquals("x:-2, y:-2, w:7, h:10", rectangleOriginal.union(unOverlappedE).toString());
+        assertEquals("x:-4, y:-2, w:6, h:5", rectangleOriginal.union(unOverlappedW).toString());
+
+        assertEquals("x:-2, y:-2, w:12, h:12", rectangleOriginal.union(unOverlappedNE).toString());
+        assertEquals("x:-8, y:-8, w:10, h:10", rectangleOriginal.union(unOverlappedNW).toString());
+        assertEquals("x:-2, y:-8, w:12, h:10", rectangleOriginal.union(unOverlappedSE).toString());
+        assertEquals("x:-8, y:-8, w:10, h:10", rectangleOriginal.union(unOverlappedSW).toString());
+
+    }
+
+    /**
+     * test rectangles touched 1/2/3 edge(s) of the original rectangle
+     */
+    @Test
+    public void testTouched() throws Exception {
+        // 1 touched edge
+        assertFalse(rectangleOriginal.overlap(touch1edge));
+        assertEquals("x:-4, y:-2, w:6, h:4", rectangleOriginal.union(touch1edge).toString());
+
+        // 2 touched edges
+        assertTrue(rectangleOriginal.overlap(touch2edge));
+        assertEquals("x:-2, y:-2, w:4, h:4", rectangleOriginal.intersect(touch2edge).toString());
+        assertEquals("x:-4, y:-5, w:6, h:8", rectangleOriginal.union(touch2edge).toString());
+
+        // 3 touched edges
+        assertTrue(rectangleOriginal.overlap(touch3edge));
+        assertEquals("x:-2, y:-2, w:4, h:4", rectangleOriginal.intersect(touch3edge).toString());
+        assertEquals("x:-2, y:-2, w:6, h:6", rectangleOriginal.union(touch3edge).toString());
+    }
+
+    @Test (expected = Exception.class)
+    public void testTouchedException() throws Exception {
+        rectangleOriginal.intersect(touch1edge);
+    }
+
+    /**
+     * test completely duplicated rectangles
+     */
+    @Test
+    public void testCompletelyOverlap() throws Exception {
+        Rectangle sameWithOriginal = new Rectangle(-2,-2,4,4);
+        Rectangle expectedResult = new Rectangle(-2, -2, 4, 4);
+
+        assertTrue(rectangleOriginal.overlap(sameWithOriginal));
+
+        assertEquals("x:-2, y:-2, w:4, h:4", rectangleOriginal.union(sameWithOriginal).toString());
+        assertEquals(expectedResult.getX(), rectangleOriginal.getX());
+        assertEquals(expectedResult.getY(), rectangleOriginal.getY());
+        assertEquals(expectedResult.getW(), sameWithOriginal.getW());
+        assertEquals(expectedResult.getH(), sameWithOriginal.getH());
+    }
+
+    /**
+     * test rectangles touched 4 corner-edges of the original rectangle + throws Exception
+     */
+    @Test
+    public void testTouchedCornerNode() throws Exception {
+        assertFalse(rectangleOriginal.overlap(touchNodeNE));
+        assertEquals("x:-2, y:-2, w:6, h:6", rectangleOriginal.union(touchNodeNE).toString());
+
+        assertFalse(rectangleOriginal.overlap(touchNodeNW));
+        assertEquals("x:-4, y:-2, w:6, h:6", rectangleOriginal.union(touchNodeNW).toString());
+
+        assertFalse(rectangleOriginal.overlap(touchNodeSE));
+        assertEquals("x:-2, y:-4, w:6, h:6", rectangleOriginal.union(touchNodeSE).toString());
+
+        assertFalse(rectangleOriginal.overlap(touchNodeSW));
+        assertEquals("x:-4, y:-4, w:6, h:6", rectangleOriginal.union(touchNodeSW).toString());
+    }
+
+    @Test (expected = Exception.class)
+    public void testTouchedCornerNodeException() throws Exception {
+        rectangleOriginal.intersect(touchNodeNE);
+        rectangleOriginal.intersect(touchNodeNW);
+        rectangleOriginal.intersect(touchNodeSE);
+        rectangleOriginal.intersect(touchNodeSW);
+    }
+
+    /**
+     * test rectangles contain and be contained by the original rectangle
+     */
+    @Test
+    public void testInNOut() throws Exception {
+        assertTrue(rectangleOriginal.overlap(innerRectangle));
+        assertEquals(innerRectangle.toString(), rectangleOriginal.intersect(innerRectangle).toString());
+        assertEquals(rectangleOriginal.toString(), rectangleOriginal.union(innerRectangle).toString());
+
+        assertTrue(rectangleOriginal.overlap(outerRectangle));
+        assertEquals(rectangleOriginal.toString(), rectangleOriginal.intersect(outerRectangle).toString());
+        assertEquals(outerRectangle.toString(), rectangleOriginal.union(outerRectangle).toString());
     }
 
 
-//        assertEquals(true, rectangleOriginal.intersect(rectangleS));
-//        assertEquals(true, rectangleOriginal.intersect(rectangleE));
-//        assertEquals(true, rectangleOriginal.intersect(rectangleW));
-
-//        assertEquals(true, rectangleOriginal.intersect(rectangleNW));
-//        assertEquals(true, rectangleOriginal.intersect(rectangleNE));
-//        assertEquals(true, rectangleOriginal.intersect(rectangleSE));
-//        assertEquals(true, rectangleOriginal.intersect(rectangleSW));
-
-//    @Test
-//    public void test
-
+// Already tested Setter/Getter & toString in above tests for your information:)
 
 
 }
