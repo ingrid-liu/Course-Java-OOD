9071e68ecc8ad005f528dcebcfc4090c1804891a
summeringrid <66650505+summeringrid@users.noreply.github.com>
2021-06-27 19:04:48 -0700

    Update GListImpl.java

--- Labs/Lab6/src/GListImpl.java
+++ Labs/Lab6/src/GListImpl.java
@@ -5,11 +5,7 @@ public class GListImpl<G> implements GList<G>{
     public GListImpl(G val) {
         this.val = val;
     }
-<<<<<<< HEAD
 
-=======
-    
->>>>>>> 815acbf4a521a2d69448a6e416ea914539534e61
     public GListImpl(G val, GListImpl<G> innerList) {
         this.val = val;
         this.innerList = innerList;
